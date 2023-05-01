2a42ed045432d838a91f0a615cb1c6bb103a37db
Kelsey Guffanti <kc.breen3@gmail.com>
2023-04-30 20:06:16 -0400

    finished step1

---
+++ step1.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const process = require("process");
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(data);
+  });
+}
+
+// this allows us to specify the path from the command line
+cat(process.argv[2]);
